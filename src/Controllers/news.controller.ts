9eeadbdbf50897a6a786ab2d4a0d8f9692d912f7
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-12-06 20:59:01 +0100

    review fixes

--- src/Controllers/news.controller.ts
+++ src/Controllers/news.controller.ts
@@ -7,39 +7,43 @@ import {
 } from 'express';
 import axios from 'axios';
 import cheerio from 'cheerio';
+import { News } from '@/Types/types';
 
 export const getNewsMFI: RequestHandler = async (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    const HTMLData = await axios
-        .get('https://mfi.ug.edu.pl/wydzial/aktualnosci')
-        .then((res) => res.data)
-        .catch((err) => {
-            res.send(err);
+    try {
+        const HTMLDataRequest = await axios.get(
+            'https://mfi.ug.edu.pl/wydzial/aktualnosci'
+        );
+        const HTMLData = HTMLDataRequest.data;
+
+        const newsArray: News[] = [];
+        const $ = cheerio.load(HTMLData);
+        const selectedElem = '.item-list > ul > li';
+        $(selectedElem).each((parentIndex, parentElem) => {
+            const img = $(parentElem)
+                .find('.image-style-news-main-page')
+                .attr('src');
+            const href = $(parentElem).find('.news-title a').attr('href');
+            const title = $(parentElem).find('.news-title a').text();
+            const datetime = $(parentElem).find('.news-datetime').text();
+            const body = $(parentElem).find('.news-body').text();
+            const newsDetail = {
+                photo: 'https://mfi.ug.edu.pl' + img,
+                link: 'https://mfi.ug.edu.pl' + href,
+                datetime: datetime,
+                title: title,
+                body: body,
+            };
+            newsArray.push(newsDetail);
         });
-    const newsArray: {}[] = [];
-    const $ = cheerio.load(HTMLData);
-    const selectedElem: string = '.item-list > ul > li';
-    $(selectedElem).each((parentIndex, parentElem) => {
-        const img = $(parentElem)
-            .find('.image-style-news-main-page')
-            .attr('src');
-        const href = $(parentElem).find('.news-title a').attr('href');
-        const title = $(parentElem).find('.news-title a').text();
-        const datetime = $(parentElem).find('.news-datetime').text();
-        const body = $(parentElem).find('.news-body').text();
-        const newsDetail = {
-            photo: 'https://mfi.ug.edu.pl' + img,
-            link: 'https://mfi.ug.edu.pl' + href,
-            datetime: datetime,
-            title: title,
-            body: body,
-        };
-        newsArray.push(newsDetail);
-    });
-    res.send(newsArray);
+        res.send(newsArray);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const getNewsINF: RequestHandler = async (
@@ -47,33 +51,39 @@ export const getNewsINF: RequestHandler = async (
     res: Response,
     next: NextFunction
 ) => {
-    const HTMLData = await axios
-        .get('https://inf.ug.edu.pl/studinfo', {
-            headers: {
-                'Accept-Encoding': 'application/json',
-            },
-        })
-        .then((res) => res.data)
-        .catch((err) => {
-            res.send(err);
+    try {
+        const HTMLDataRequest = await axios.get(
+            'https://inf.ug.edu.pl/studinfo',
+            {
+                headers: {
+                    'Accept-Encoding': 'application/json',
+                },
+            }
+        );
+        const HTMLData = HTMLDataRequest.data;
+        const newsArray: News[] = [];
+        const $ = cheerio.load(HTMLData);
+        const selectedElem = 'div.newsBox';
+        $(selectedElem).each((parentIndex, parentElem) => {
+            const img = $(parentElem)
+                .find('.newsThumb')
+                .find('img')
+                .attr('src');
+            const href = $(parentElem).find('a').first().attr('href');
+            const title = $(parentElem).find('.newsTitle a').text();
+            const datetime = $(parentElem).find('.newsDate').text();
+            const body = $(parentElem).find('.newsBody').text();
+            const newsDetail = {
+                photo: 'https://inf.ug.edu.pl/' + img,
+                link: 'https://inf.ug.edu.pl/' + href,
+                datetime: datetime,
+                title: title,
+                body: body,
+            };
+            newsArray.push(newsDetail);
         });
-    const newsArray: {}[] = [];
-    const $ = cheerio.load(HTMLData);
-    const selectedElem: string = 'div.newsBox';
-    $(selectedElem).each((parentIndex, parentElem) => {
-        const img = $(parentElem).find('.newsThumb').find('img').attr('src');
-        const href = $(parentElem).find('a').first().attr('href');
-        const title = $(parentElem).find('.newsTitle a').text();
-        const datetime = $(parentElem).find('.newsDate').text();
-        const body = $(parentElem).find('.newsBody').text();
-        const newsDetail = {
-            photo: 'https://inf.ug.edu.pl/' + img,
-            link: 'https://inf.ug.edu.pl/' + href,
-            datetime: datetime,
-            title: title,
-            body: body,
-        };
-        newsArray.push(newsDetail);
-    });
-    res.send(newsArray);
+        res.send(newsArray);
+    } catch (error) {
+        res.send(error);
+    }
 };
