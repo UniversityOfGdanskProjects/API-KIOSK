34633355cd68717f37a1245a639aee994a003af3
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-12-08 00:20:01 +0100

    review fixes

--- src/Controllers/news.controller.ts
+++ src/Controllers/news.controller.ts
@@ -20,32 +20,35 @@ export const getNewsMFI: RequestHandler = async (
         );
         const HTMLData = HTMLDataRequest.data;
 
-        const newsArray: News[] = [];
         const $ = cheerio.load(HTMLData);
         const selectedElem = '.item-list > ul > li';
-        $(selectedElem).map((parentIndex, parentElem) => {
-            const img = $(parentElem)
-                .find('.image-style-news-main-page')
-                .attr('src');
-            const href = $(parentElem).find('.news-title a').attr('href');
-            const title = $(parentElem).find('.news-title a').text();
-            const datetime = $(parentElem).find('.news-datetime').text();
-            const body = $(parentElem).find('.news-body').text();
-            const newsDetail = {
-                photo: 'https://mfi.ug.edu.pl' + img,
-                link: 'https://mfi.ug.edu.pl' + href,
-                datetime: datetime,
-                title: title,
-                body: body,
-            };
-            newsArray.push(newsDetail);
-        });
+        const newsArray: News[] = $(selectedElem)
+            .map((parentIndex, parentElem) => {
+                const img = $(parentElem)
+                    .find('.image-style-news-main-page')
+                    .attr('src');
+                const href = $(parentElem).find('.news-title a').attr('href');
+                const title = $(parentElem).find('.news-title a').text();
+                const datetime = $(parentElem).find('.news-datetime').text();
+                const body = $(parentElem).find('.news-body').text();
+                const newsDetail = {
+                    photo: 'https://mfi.ug.edu.pl' + img,
+                    link: 'https://mfi.ug.edu.pl' + href,
+                    datetime: datetime,
+                    title: title,
+                    body: body,
+                };
+
+                return newsDetail;
+            })
+            .get();
+
         res.send(newsArray);
     } catch (err) {
         const error = err as Error | AxiosError;
         res.status(500).json({
             success: false,
-            message: 'Błąd serwera!',
+            message: 'Could not get data!',
             error: error.message,
         });
     }
@@ -66,33 +69,35 @@ export const getNewsINF: RequestHandler = async (
             }
         );
         const HTMLData = HTMLDataRequest.data;
-        const newsArray: News[] = [];
         const $ = cheerio.load(HTMLData);
         const selectedElem = 'div.newsBox';
-        $(selectedElem).map((parentIndex, parentElem) => {
-            const img = $(parentElem)
-                .find('.newsThumb')
-                .find('img')
-                .attr('src');
-            const href = $(parentElem).find('a').first().attr('href');
-            const title = $(parentElem).find('.newsTitle a').text();
-            const datetime = $(parentElem).find('.newsDate').text();
-            const body = $(parentElem).find('.newsBody').text();
-            const newsDetail = {
-                photo: 'https://inf.ug.edu.pl/' + img,
-                link: 'https://inf.ug.edu.pl/' + href,
-                datetime: datetime,
-                title: title,
-                body: body,
-            };
-            newsArray.push(newsDetail);
-        });
+        const newsArray: News[] = $(selectedElem)
+            .map((parentIndex, parentElem) => {
+                const img = $(parentElem)
+                    .find('.newsThumb')
+                    .find('img')
+                    .attr('src');
+                const href = $(parentElem).find('a').first().attr('href');
+                const title = $(parentElem).find('.newsTitle a').text();
+                const datetime = $(parentElem).find('.newsDate').text();
+                const body = $(parentElem).find('.newsBody').text();
+                const newsDetail = {
+                    photo: 'https://inf.ug.edu.pl/' + img,
+                    link: 'https://inf.ug.edu.pl/' + href,
+                    datetime: datetime,
+                    title: title,
+                    body: body,
+                };
+                return newsDetail;
+            })
+            .get();
+
         res.send(newsArray);
     } catch (err) {
         const error = err as Error | AxiosError;
         res.status(500).json({
             success: false,
-            message: 'Błąd serwera!',
+            message: 'Could not get data!',
             error: error.message,
         });
     }
