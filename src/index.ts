4084b17cdc5e07bf57ef10efa55e6770488b5538
Aleksandra Mordzon <a.mordzon.694@studms.ug.edu.pl>
2022-12-11 21:21:10 +0100

    Added news tests

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app: Express = express();
-const port = process.env.PORT;
 
 app.use('/news', newsRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+export default app;
