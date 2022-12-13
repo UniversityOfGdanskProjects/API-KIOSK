d29898ea7956289638798ae100831690cf39a82b
Aleksandra Mordzon <aleksandramordzon@Aleksandras-MacBook-Air.local>
2022-12-13 23:19:12 +0100

    Connect to database

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,21 @@ import 'module-alias/register';
 import newsRouter from './Routes/news.route';
 import schedule from 'node-schedule';
 import { NewsScheduler } from './Services/scheduler.service';
+import mongoose, { ConnectOptions } from 'mongoose';
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
-
+mongoose.set('strictQuery', true);
 app.use('/news', newsRouter);
+mongoose.connect(
+    'mongodb+srv://' +
+        process.env.MONGO_USER +
+        ':' +
+        process.env.MONGO_PASSWORD +
+        process.env.MONGO_PATH
+);
 
 schedule.scheduleJob('* * * * *', NewsScheduler);
 
