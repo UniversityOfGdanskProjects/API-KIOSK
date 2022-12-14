7b5cec7c741391d10da5674a558e8ef2d1b9a11e
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-14 22:04:44 +0100

    add mongoose to dependencies, establish basic connection with database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
+
 const dotenv = require('dotenv');
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
+
+const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_PATH}`;
+
+mongoose.connect(connectionString);
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+
+db.once('connected', () => console.log('MongoDB connected succesfully.'));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
