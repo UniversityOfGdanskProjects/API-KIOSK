8526f8342cf811c189b3b086b6736c97ec161a61
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-15 19:20:18 +0100

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ const port = process.env.PORT;
 const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_PATH}`;
 
 mongoose.set('strictQuery', false);
-mongoose.connect(connectionString);
-
-const db = mongoose.connection;
+mongoose
+    .connect(connectionString)
+    .then(() => console.log('Connected with MongoDB'));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
