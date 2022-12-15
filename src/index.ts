26a425339462a9397b7093a75a7505f2247bb4dd
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-15 19:05:51 +0100

    fix db connection

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,16 @@ import mongoose from 'mongoose';
 const dotenv = require('dotenv');
 
 dotenv.config();
+
 const app: Express = express();
 const port = process.env.PORT;
 
 const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_PATH}`;
 
+mongoose.set('strictQuery', false);
 mongoose.connect(connectionString);
 
 const db = mongoose.connection;
-
-db.on('error', (error) => console.log(error));
-
-db.once('connected', () => console.log('MongoDB connected succesfully.'));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
