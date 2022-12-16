9aecf3594c124b66252d69d584be71ee800b9ac8
Wojciech Gola <91955404+wgola@users.noreply.github.com>
2022-12-16 13:31:22 +0100

    build: connect with database (#19)
    * add mongoose to dependencies, establish basic connection with database
    
    * fix db connection
    
    * fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
+
 const dotenv = require('dotenv');
 
 dotenv.config();
+
 const app: Express = express();
 const port = process.env.PORT;
+
+const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_PATH}`;
+
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(connectionString)
+    .then(() => console.log('Connected with MongoDB'));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
