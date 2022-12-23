4e096a609c44136f9b0fbf5a07058fdcf51d45d3
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2022-12-23 14:09:31 +0100

    created type and model for users collection

---
+++ src/Models/users.model.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { Users } from '../Types/Users';
+
+const UsersSchema = new mongoose.Schema<Users>({
+    login: {
+        type: String,
+        required: true,
+        unique: true,
+        dropDups: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+export const UsersModel = mongoose.model<Users>('Users', UsersSchema, 'users');
---
+++ src/Types/Users.ts
@@ -0,0 +1,4 @@
+export interface Users {
+    login: string;
+    password: string;
+}
