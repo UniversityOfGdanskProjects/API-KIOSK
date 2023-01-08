5080c33f72fc1ba234af32c2bdfab5c80a33b440
Jakub Zapiórkowski <zapiorkowski.j@gmail.com>
2023-01-08 15:20:05 +0100

    unit test

--- src/utils/checkForHtmlInText.ts
+++ src/utils/checkForHtmlInText.ts
@@ -1,5 +1,5 @@
 export const checkForHtmlInText = (text: string): boolean => {
-    const htmlElements = ['<a>', '<li>'];
+    const htmlElements = ['<a ', '<li>'];
 
     return htmlElements.some((element) => text.includes(element));
 };
