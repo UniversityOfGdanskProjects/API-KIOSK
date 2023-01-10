d4ca397a5a033fc827dcb33f7c85ddb15ca5b98c
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-01-10 21:02:39 +0100

    fixed tests

--- tests/Routes/login.route.test.ts
+++ tests/Routes/login.route.test.ts
@@ -16,14 +16,14 @@ describe('Test of login route', () => {
 
     it.only('Correct login data', async () => {
         const loginData = {
-            login: 'login_routes',
+            login: 'login_route',
             password: 'password',
         };
 
         const response = await request(app).post('/login').send(loginData);
 
         expect(response.status).toEqual(200);
-        expect(response.body.userData.login).toEqual('login_routes');
+        expect(response.body.userData.login).toEqual('login_route');
         expect(response.body.userData.password).toEqual(hashedPassword);
         expect(response.body).toHaveProperty('jwt_token');
     });
