ec533fb4b7bdf468103ce0abdd1181739477af0d
Wojciech Gola <91955404+wgola@users.noreply.github.com>
2023-01-12 10:36:18 +0100

    test: test login route (#31)
    * refactored index.ts, created login.route.test.ts
    
    * refactored tests for login feature
    
    * fixed tests
    
    * refactored tests
    
    * refactored tests
    
    * small fixes

---
+++ tests/Routes/login.route.test.ts
@@ -0,0 +1,53 @@
+import { connectToDB } from '../../src/Configs/db.config';
+import { UsersModel } from '../../src/Models/users.model';
+import { app, server } from '../../src/index';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import bcrypt from 'bcrypt';
+
+describe('Test of login route', () => {
+    let hashedPassword: string;
+
+    beforeAll(async () => {
+        hashedPassword = await bcrypt.hash('password', 10);
+
+        await connectToDB();
+        await UsersModel.create({
+            login: 'login_route',
+            password: hashedPassword,
+        });
+    });
+
+    afterAll(async () => {
+        await UsersModel.deleteOne({ login: 'login_route' });
+        await mongoose.disconnect();
+
+        server.close();
+    });
+
+    it.only('Correct login data', async () => {
+        const loginData = {
+            login: 'login_route',
+            password: 'password',
+        };
+
+        const response = await request(app).post('/login').send(loginData);
+
+        expect(response.status).toEqual(200);
+        expect(response.body.userData.login).toEqual('login_route');
+        expect(response.body.userData.password).toEqual(hashedPassword);
+        expect(response.body).toHaveProperty('jwt_token');
+    });
+
+    it.only('Wrong login data', async () => {
+        const loginData = {
+            login: 'anotherLogin',
+            password: 'anotherPassword',
+        };
+
+        const response = await request(app).post('/login').send(loginData);
+
+        expect(response.status).toEqual(401);
+        expect(response.text).toEqual('Invalid login or password.');
+    });
+});
--- tests/Services/login.service.test.ts
+++ tests/Services/login.service.test.ts
@@ -1,17 +1,16 @@
-import { UsersModel } from '../../src/Models/users.model';
 import { checkLoginData } from '../../src/Services/login.service';
 import { connectToDB } from '../../src/Configs/db.config';
-import bcrypt from 'bcrypt';
+import { UsersModel } from '../../src/Models/users.model';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 describe('Test of login service', () => {
     let hashedPassword: string;
 
     beforeAll(async () => {
-        await connectToDB();
-
         hashedPassword = await bcrypt.hash('password', 10);
 
+        await connectToDB();
         await UsersModel.create({
             login: 'login_service',
             password: hashedPassword,
