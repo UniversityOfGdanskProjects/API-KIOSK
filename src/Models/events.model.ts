fe2da68f374a85615a526b6dd8fb0634c8a51165
Konrad Sokolowski <s.konrad103@gmail.com>
2023-01-26 01:37:43 +0100

    nested content text removed, slight changes in scraper

--- src/Models/events.model.ts
+++ src/Models/events.model.ts
@@ -3,7 +3,7 @@ import { Events } from '../Types/events.type';
 
 const EventsSchema = new mongoose.Schema<Events>({
     name: { type: String },
-    link: { type: String },
+    url: { type: String },
     content: {
         header: { type: String },
         text: [
