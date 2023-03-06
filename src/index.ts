a3d4adf09fe7e0857de7f7993e38a1e013967613
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-03-06 16:50:11 +0100

    refactored index.ts and app.ts, added port to environment

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,20 @@
-import express, { Express } from 'express';
-import { connectToDB } from './Configs/db.config';
-import cors from 'cors';
-import majorsRouter from './Routes/majors.route';
-import loginRouter from './Routes/login.route';
-import lessonsPlansRouter from './Routes/lessonsPlans.route';
-import staffRouter from './Routes/staff.route';
-import schedule from 'node-schedule';
 import { updateLessons } from './Schedulers/lessonsPlans.scheduler';
-import { updateStaff } from './Schedulers/staffScheduler';
 import { updateMajors } from './Schedulers/majors.scheduler';
+import { updateStaff } from './Schedulers/staffScheduler';
+import { connectToDB } from './Configs/db.config';
+import schedule from 'node-schedule';
+import { app } from './app';
 
-const app: Express = express();
-const port = 3001;
+const PORT = process.env.PORT;
 
 connectToDB();
-
-app.use(express.json());
-app.use(cors());
-
-app.use(majorsRouter);
-app.use(loginRouter);
-app.use(lessonsPlansRouter);
-app.use(staffRouter);
 
 schedule.scheduleJob('lessonsPlanUpdate', '0 0 * * *', updateLessons);
 schedule.scheduleJob('staffUpdate', '0 0 * * *', updateStaff);
 schedule.scheduleJob('majorsUpdate', '0 0 * * *', updateMajors);
 
-const server = app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+const server = app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
-export { app, server };
+export { server };
