ef14be3c16f234932ecfa7be968a2bdfdec1f10c
justyna <towarnickaj@gmail.com>
2023-03-22 22:37:57 +0100

    pagination + search feature

--- src/Controllers/staff.controller.ts
+++ src/Controllers/staff.controller.ts
@@ -15,16 +15,18 @@ export const getStaff = async (
 ) => {
     try {
         const { page, name } = req.query;
+        const criteria = name ? { name: { $regex: name, $options: 'i' } } : {};
         const pageNumber = page ? parseInt(page, 10) : 1;
-        const staff = await StaffModel.find(
-            { name: { $regex: name, $options: 'i' } },
-            { __v: 0, 'content.posts._id': 0 }
-        )
+        const staff = await StaffModel.find(criteria, {
+            __v: 0,
+            'content.posts._id': 0,
+        })
             .skip((pageNumber - 1) * 30)
             .limit(30);
 
-        const totalRecords = await StaffModel.countDocuments();
-        const totalPages = Math.ceil(totalRecords / 30);
+        const totalStaffRecords = await StaffModel.countDocuments(criteria);
+        const totalPages = Math.ceil(totalStaffRecords / 30);
+
         return res
             .status(200)
             .json({ content: staff, page: pageNumber, totalPages });
@@ -47,28 +49,6 @@ export const getStaffById: RequestHandler = async (
         });
 
         return res.status(200).json(person);
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ message: `There's an ${error}. Please try again.` });
-    }
-};
-
-export const searchStaffByName: RequestHandler = async (
-    req: Request<any, any, { name: string }>,
-    res: Response<Academic[] | { message: string }>,
-    next: NextFunction
-) => {
-    try {
-        const { name } = req.query;
-        const criteria = name ? { name: { $regex: name, $options: 'i' } } : {};
-
-        const staff = await StaffModel.find(criteria, {
-            __v: 0,
-            'content.posts._id': 0,
-        });
-
-        return res.status(200).json(staff);
     } catch (error) {
         return res
             .status(500)
--- src/Routes/staff.route.ts
+++ src/Routes/staff.route.ts
@@ -1,14 +1,9 @@
-import {
-    getStaff,
-    getStaffById,
-    searchStaffByName,
-} from '../Controllers/staff.controller';
+import { getStaff, getStaffById } from '../Controllers/staff.controller';
 import { Router } from 'express';
 
 const staffRouter = Router();
 
 staffRouter.get('/staff', getStaff);
-staffRouter.get('/staff/search', searchStaffByName);
 staffRouter.get('/staff/:id', getStaffById);
 
 export default staffRouter;
