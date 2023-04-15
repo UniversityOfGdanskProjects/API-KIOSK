d915632a9fd100f2b8b6b05c193afffe7b51af5a
Wojciech Gola <wojciech.maciej.gola@wp.pl>
2023-04-15 12:46:28 +0200

    refactored lessons plans web scrapper

--- src/Services/lessonsPlansScrapper.service.ts
+++ src/Services/lessonsPlansScrapper.service.ts
@@ -1,229 +1,270 @@
-import { checkIfFaculty } from '../utils/lessonsPlanScrapper/checkIfFaculty';
 import { LessonsPlanURLObject } from '../Types/lessonsPlanURLObject.type';
-import { LessonsPlanEntry } from '../Types/lessonsPlanEntry.type';
 import { PlanEntryFromCSV } from '../Types/planEntryFromCSV.type';
+import { LessonsPlanEntry } from '../Types/lessonsPlanEntry.type';
+import { returnScraperError } from '../utils/errorScraper';
+import { ErrorType } from '../Types/error.type';
 import {
-    getYearFromMajorName,
+    checkIfFaculty,
     getURLWithoutQuery,
+    getYearFromMajorName,
+    transformCSVField,
     transformCSVHeader,
     transformGroupName,
-    transformCSVField,
 } from '../utils/lessonsPlanScrapper';
 import cheerio from 'cheerio';
 import Papa from 'papaparse';
-import * as _ from 'lodash';
 import axios from 'axios';
-import { ErrorType } from 'Types/error.type';
-import { returnScraperError } from '../utils/errorScraper';
 
-export const getDataFromCSV = async (
-    url: string,
-    classType: 'main' | 'seminar' | 'faculty',
-    majorName: string
-): Promise<LessonsPlanEntry[]> => {
-    try {
-        const mapResult = (
-            entryFromCSV: PlanEntryFromCSV,
-            mainGroupName: string
-        ): LessonsPlanEntry => {
-            const {
-                subject,
-                groups,
-                type,
-                info,
-                start_date,
-                end_date,
-                ...rest
-            } = entryFromCSV;
+const MAIN_URL = 'https://inf.ug.edu.pl/plan/';
 
-            const { name, year } = getYearFromMajorName(majorName);
-
-            const entryGroup = groups
-                ? groups.map((group) =>
-                      transformGroupName(mainGroupName, group)
-                  )
-                : [];
-
-            if (entryGroup.length === 0)
-                entryGroup.push(
-                    classType === 'faculty' ? 'fakultet' : 'seminarium'
-                );
-
-            const result: LessonsPlanEntry = {
-                name: name,
-                year: year,
-                ...rest,
-                groups: entryGroup,
-                pl: {
-                    subject: subject,
-                    type: type,
-                    info: info,
-                },
-                eng: {},
-            };
-
-            return result;
-        };
-
-        const parserConfig = {
-            header: true,
-            dynamicTyping: true,
-            transformHeader: transformCSVHeader,
-            transform: transformCSVField,
-        };
-
-        const csv = await axios.get(
-            `https://inf.ug.edu.pl/plan/${url}&format=csv`
-        );
-
-        const results: PlanEntryFromCSV[] = Papa.parse<PlanEntryFromCSV>(
-            csv.data,
-            parserConfig
-        ).data;
-
-        const withoutEmptyLines = results.filter(
-            (result) => Object.keys(result).length > 1
-        );
-
-        const finalResults = withoutEmptyLines.map((result) => {
-            if (
-                url.startsWith('fiz/') &&
-                !majorName.includes('bezpieczeństwo jądrowe')
-            ) {
-                const [, mainGroupName] = url.split('=');
-
-                return mapResult(result, mainGroupName);
-            }
-
-            return mapResult(result, majorName);
-        });
-
-        return finalResults;
-    } catch (error) {
-        return [];
-    }
-};
-
-const getAllUrls = async (
-    currentURL: string
-): Promise<LessonsPlanURLObject> => {
-    try {
-        const mainURL = 'https://inf.ug.edu.pl/plan/';
-
-        const { data } = await axios.get(mainURL + currentURL);
-        const $ = cheerio.load(data);
-        const name = $('div > h1').text();
-
-        const planURLObject: LessonsPlanURLObject = {
-            name: name,
-            main: currentURL,
-        };
-
-        const otherURLs = $('div.uwaga > a')
-            .map((index, element) => element.attribs.href)
-            .get();
-
-        for (let i = 0; i < 2; i++) {
-            if (otherURLs[i]) {
-                const url = getURLWithoutQuery(otherURLs[i]);
-                if (checkIfFaculty(url)) planURLObject.faculty = url;
-                else planURLObject.seminar = url;
-            }
-        }
-
-        return planURLObject;
-    } catch {
-        return { name: '', main: '' };
-    }
-};
-
-const getPlansURLs = async (): Promise<LessonsPlanURLObject[]> => {
-    try {
-        const url = 'https://inf.ug.edu.pl/plan/';
-
-        const mainLessonsPlansPage = await axios.get(url + '?dialog=grupa');
-
-        const $1 = cheerio.load(mainLessonsPlansPage.data);
-
-        const majorsURLs = $1('#groups > div > a')
-            .map((index, element) => element.attribs.href)
-            .get();
-
-        const mainMajorsURLs = majorsURLs.splice(0, majorsURLs.length - 1);
-
-        const otherLessonsPlansPage = await axios.get(url + 'fiz/');
-
-        const $2 = cheerio.load(otherLessonsPlansPage.data);
-
-        const otherURls = $2('#groups > div > a')
-            .map((index, element) => 'fiz/' + element.attribs.href)
-            .get();
-
-        const otherMajorsURLs = otherURls.splice(0, otherURls.length - 3);
-
-        const allMajorsPlanURLs = await Promise.all(
-            [...mainMajorsURLs, ...otherMajorsURLs].map(getAllUrls)
-        );
-
-        return allMajorsPlanURLs;
-    } catch (error) {
-        return [];
-    }
-};
-
-export const lessonPlansScrapper = async (): Promise<
+export const lessonsPlansScrapper = async (): Promise<
     LessonsPlanEntry[] | ErrorType
 > => {
     try {
         const plansURLObjects = await getPlansURLs();
 
-        if (!plansURLObjects)
-            return {
-                status: 500,
-                message: "Couldn't find any lessons plans URLs",
-            };
+        if (plansURLObjects.length === 0)
+            throw new Error("Couldn't find any URLs");
 
         const allLessonsPlans = await Promise.all(
             plansURLObjects.map(async (url: LessonsPlanURLObject) => {
-                const allLessons = [];
-
-                const mainLessons = await getDataFromCSV(
-                    url.main,
-                    'main',
-                    url.name
-                );
-                allLessons.push(...mainLessons);
-
-                if (url.faculty) {
-                    const facultyLessons = await getDataFromCSV(
-                        url.faculty,
-                        'faculty',
-                        url.name
-                    );
-                    allLessons.push(...facultyLessons);
-                }
-
-                if (url.seminar) {
-                    const seminarLessons = await getDataFromCSV(
-                        url.seminar,
-                        'seminar',
-                        url.name
-                    );
-                    allLessons.push(...seminarLessons);
-                }
-
-                return allLessons;
+                return await getDataFromCSV(url);
             })
         );
 
-        const result = allLessonsPlans.reduce((acc, lessonsArray) => {
+        const finalResult = allLessonsPlans.reduce((acc, lessonsArray) => {
             acc.push(...lessonsArray);
-
             return acc;
         }, []);
 
-        return result;
-    } catch (error: any) {
+        console.log(finalResult);
+
+        return finalResult;
+    } catch (error) {
         return returnScraperError(error);
     }
+};
+
+const getPlansURLs = async (): Promise<LessonsPlanURLObject[]> => {
+    try {
+        const firstLessonsPlansPage = await axios.get(
+            MAIN_URL + '?dialog=grupa'
+        );
+
+        const $1 = cheerio.load(firstLessonsPlansPage.data);
+
+        const allURLsFromFirstPage = $1('#groups > div > a')
+            .map((_, element) => element.attribs.href)
+            .get();
+
+        const mainMajorsURLs = allURLsFromFirstPage.splice(
+            0,
+            allURLsFromFirstPage.length - 1
+        );
+
+        const secondLessonsPlansPage = await axios.get(MAIN_URL + 'fiz/');
+
+        const $2 = cheerio.load(secondLessonsPlansPage.data);
+
+        const allURLsFromSecondPage = $2('#groups > div > a')
+            .map((_, element) => 'fiz/' + element.attribs.href)
+            .get();
+
+        const otherMajorsURLs = allURLsFromSecondPage.splice(
+            0,
+            allURLsFromSecondPage.length - 3
+        );
+
+        const allLessonsPlansURLObjects = await Promise.all(
+            [...mainMajorsURLs, ...otherMajorsURLs].map(getAllURLsForMajor)
+        );
+
+        return allLessonsPlansURLObjects;
+    } catch (error) {
+        return [];
+    }
+};
+
+const getAllURLsForMajor = async (
+    currentURL: string
+): Promise<LessonsPlanURLObject> => {
+    try {
+        const lessonsPlanPage = await axios.get(MAIN_URL + currentURL);
+
+        const $ = cheerio.load(lessonsPlanPage.data);
+        const majorName = $('div > h1').text();
+
+        const planURLObject: LessonsPlanURLObject = {
+            majorName: majorName,
+            mainURL: currentURL,
+        };
+
+        const otherURLs = $('div.uwaga > a')
+            .map((_, element) => element.attribs.href)
+            .get();
+
+        for (let i = 0; i < 2; i++) {
+            if (otherURLs[i]) {
+                const url = getURLWithoutQuery(otherURLs[i]);
+                if (checkIfFaculty(url)) planURLObject.facultyURL = url;
+                else planURLObject.seminarURL = url;
+            }
+        }
+
+        return planURLObject;
+    } catch {
+        return { majorName: '', mainURL: '' };
+    }
+};
+
+const getDataFromCSV = async (
+    majorURLObject: LessonsPlanURLObject
+): Promise<LessonsPlanEntry[]> => {
+    const parserConfig = {
+        header: true,
+        dynamicTyping: true,
+        transformHeader: transformCSVHeader,
+        transform: transformCSVField,
+    };
+
+    const getData = async (URL: string) => {
+        const csv = await axios.get(`${MAIN_URL}${URL}&format=csv`);
+        const results: PlanEntryFromCSV[] = Papa.parse<PlanEntryFromCSV>(
+            csv.data,
+            parserConfig
+        ).data;
+        const resultsWithoutEmptyLines = results.filter(
+            (result) => Object.keys(result).length > 1
+        );
+
+        return resultsWithoutEmptyLines;
+    };
+
+    const mapMajorResults = (
+        result: PlanEntryFromCSV,
+        url: string,
+        classType: 'main' | 'seminar' | 'faculty'
+    ): LessonsPlanEntry => {
+        if (
+            url.startsWith('fiz/') &&
+            !majorURLObject.majorName.includes('bezpieczeństwo jądrowe')
+        ) {
+            const [, mainGroupName] = url.split('=');
+
+            return mapResults(
+                result,
+                mainGroupName,
+                majorURLObject.majorName,
+                classType
+            );
+        }
+
+        return mapResults(
+            result,
+            majorURLObject.majorName,
+            majorURLObject.majorName,
+            classType
+        );
+    };
+
+    const finalResults: LessonsPlanEntry[] = [];
+    const mainData = await getData(majorURLObject.mainURL);
+
+    const mainDataResults = mainData.map((result) =>
+        mapMajorResults(result, majorURLObject.mainURL, 'main')
+    );
+
+    finalResults.push(...mainDataResults);
+
+    if (majorURLObject.facultyURL) {
+        const facultyData = await getData(majorURLObject.facultyURL);
+        const facultyDataResults = facultyData.map((result) =>
+            mapMajorResults(result, majorURLObject.facultyURL || '', 'faculty')
+        );
+
+        finalResults.push(...facultyDataResults);
+    }
+
+    if (majorURLObject.seminarURL) {
+        const seminarData = await getData(majorURLObject.seminarURL);
+        const seminarDataResults = seminarData.map((result) =>
+            mapMajorResults(result, majorURLObject.seminarURL || '', 'seminar')
+        );
+
+        finalResults.push(...seminarDataResults);
+    }
+
+    const finalResultsWithChangedGroups = changeGroupNames(finalResults);
+
+    return finalResultsWithChangedGroups;
+};
+
+const changeGroupNames = (
+    lessonsPlanEntries: LessonsPlanEntry[]
+): LessonsPlanEntry[] => {
+    const allGroupNames = lessonsPlanEntries.reduce(
+        (acc1: string[], lessonEntry: LessonsPlanEntry) => {
+            const ignoredGroups = ['all', 'fakultet', 'seminarium', ...acc1];
+            const uniqueGroupNames = lessonEntry.groups.reduce(
+                (acc2: string[], group: string) => {
+                    if (!ignoredGroups.includes(group)) {
+                        acc2.push(group);
+                    }
+
+                    return acc2;
+                },
+                []
+            );
+            acc1.push(...uniqueGroupNames);
+
+            return acc1;
+        },
+        []
+    );
+
+    const lessonsPlansWithChangedGroups = lessonsPlanEntries.map(
+        (lessonsPlanEntry) => {
+            if (lessonsPlanEntry.groups[0] === 'all') {
+                lessonsPlanEntry.groups = allGroupNames;
+            }
+
+            return lessonsPlanEntry;
+        }
+    );
+
+    return lessonsPlansWithChangedGroups;
+};
+
+const mapResults = (
+    entryFromCSV: PlanEntryFromCSV,
+    mainGroupName: string,
+    majorName: string,
+    classType: 'main' | 'seminar' | 'faculty'
+): LessonsPlanEntry => {
+    const { subject, groups, type, info, start_date, end_date, ...rest } =
+        entryFromCSV;
+
+    const { name, year } = getYearFromMajorName(majorName);
+
+    const entryGroup = groups
+        ? groups.map((group) => transformGroupName(mainGroupName, group))
+        : [];
+
+    if (entryGroup.length === 0)
+        entryGroup.push(classType === 'faculty' ? 'fakultet' : 'seminarium');
+
+    const result: LessonsPlanEntry = {
+        name: name,
+        year: year,
+        ...rest,
+        groups: entryGroup,
+        pl: {
+            subject: subject,
+            type: type,
+            info: info,
+        },
+        eng: {},
+    };
+
+    return result;
 };
