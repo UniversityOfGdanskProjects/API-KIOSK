9c14f0fe9ce88400ac4ccd99b9ff83914b5dcd1b
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-11-25 21:40:01 +0100

    improve regex

--- src/utils/removeHTMLAttributes.ts
+++ src/utils/removeHTMLAttributes.ts
@@ -1,7 +1,5 @@
 export const removeHTMLAttributes = (htmlContent: string | null): string => {
     const noATag = htmlContent!.replace(/<\/?a[^>]*>/gi, '');
-    const noStyles = noATag.replace(/style="[^\"]*"/gi, '');
-    const noAlignment = noStyles.replace(/(?:align)=[^>]*/gi, '');
-    const removeSpace = noAlignment.replace(/\s+(?=>)/g, '');
-    return removeSpace;
+    const noStyles = noATag.replace(/<\s*(\w+).*?(?<!\/)>/g, '<$1>');
+    return noStyles;
 };
