78341884a45f4566cdc11c6230a27214c0b47856
amordzon <97406794+amordzon@users.noreply.github.com>
2023-11-26 12:20:47 +0100

    Fix: news ellipsis (#57)
    * remove any character that is not alfanum
    
    * changed shortBody type and regex for ellipsis
    
    * added tests
    
    * changed scheduler time
    
    * remove unecessary variable

--- src/utils/newsScraper/removeSeeMore.ts
+++ src/utils/newsScraper/removeSeeMore.ts
@@ -1,5 +1,5 @@
 export const removeSeeMore = (text: string) => {
-    const regex = /([,|\s|-])*… więcej$/;
+    const regex = /[^\wąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*… więcej$/;
     const newText = text.replace(regex, '...');
     return newText;
 };
