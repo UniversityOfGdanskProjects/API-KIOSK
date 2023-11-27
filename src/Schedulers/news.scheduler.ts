e455d3e2c90e4a436ef88fd6f75d442b1409093d
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-11-27 22:41:13 +0100

    make it more generic

--- src/Schedulers/news.scheduler.ts
+++ src/Schedulers/news.scheduler.ts
@@ -1,49 +1,10 @@
-import { News } from './../Types/News.type';
-import { NewsModel } from '../Models/news.model';
 import {
     newsScraperINF,
     newsScraperMFI,
 } from '../Services/newsScraper.service';
 import _ from 'lodash';
-
-const isDifferent = (ObjectA: News, ObjectB: News) => {
-    const compareValues = ['shortBody', 'body', 'photos', 'category'];
-    const cleanObjectA = _.pick(ObjectA, compareValues);
-    const cleanObjectB = _.pick(ObjectB, compareValues);
-    return !_.isEqual(cleanObjectA, cleanObjectB);
-};
-
-const updateOrCreateNews = async (news: News) => {
-    const filter = { title: news.title };
-
-    const existingNews = await NewsModel.findOne(filter);
-
-    if (existingNews) {
-        const isAnyFieldDifferent = isDifferent(existingNews, news);
-
-        if (isAnyFieldDifferent) {
-            await NewsModel.findOneAndUpdate(
-                filter,
-                {
-                    $set: {
-                        leadingPhoto: news.leadingPhoto,
-                        photos: news.photos,
-                        link: news.link,
-                        datetime: news.datetime,
-                        shortBody: news.shortBody,
-                        body: news.body,
-                        source: news.source,
-                        category: news.category,
-                    },
-                },
-                { new: true }
-            );
-        }
-    } else {
-        const newNews = new NewsModel(news);
-        await newNews.save();
-    }
-};
+import { createUpdateFunction } from '../utils/updatingData';
+import { NewsModel } from '../Models/news.model';
 
 export const updateNews = async () => {
     try {
@@ -52,15 +13,25 @@ export const updateNews = async () => {
 
         const uniqueNewsINF = _.uniqBy(newsINF, 'title');
         const uniqueNewsMFI = _.uniqBy(newsMFI, 'title');
+        const updateOrCreateNews = createUpdateFunction(NewsModel);
+        const compareValues = ['shortBody', 'body', 'photos', 'category'];
 
         if (uniqueNewsINF && uniqueNewsINF.length > 0) {
             uniqueNewsINF.forEach(async (news) => {
-                await updateOrCreateNews(news);
+                await updateOrCreateNews(
+                    { title: news.title },
+                    news,
+                    compareValues
+                );
             });
         }
         if (uniqueNewsMFI && uniqueNewsMFI.length > 0) {
             uniqueNewsMFI.forEach(async (news) => {
-                await updateOrCreateNews(news);
+                await updateOrCreateNews(
+                    { title: news.title },
+                    news,
+                    compareValues
+                );
             });
         }
     } catch (error) {
