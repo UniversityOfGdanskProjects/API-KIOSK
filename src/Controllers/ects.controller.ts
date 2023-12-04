ec3a835282054fb3314d657a1325820ecdc2b877
Oskar Kusmierek <oskar.kusmierekk@gmail.com>
2023-12-04 09:29:01 -0800

    fix

--- src/Controllers/ects.controller.ts
+++ src/Controllers/ects.controller.ts
@@ -1,7 +1,11 @@
 import { Request, RequestHandler, Response } from 'express';
 import { EctsModel } from '../Models/ectsSubject.model';
+import { ErrorType } from 'Types/error.type';
 
-export const getAllMajors: RequestHandler = async (req: Request, res: any) => {
+export const getAllMajors: RequestHandler = async (
+    req: Request,
+    res: Response<string[] | Partial<ErrorType>>
+) => {
     try {
         const majors = await EctsModel.find().distinct('major');
 
