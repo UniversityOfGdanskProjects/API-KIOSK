666fbfc71b493f054394c475744e21eb15bc80d7
amordzon <a.mordzon.694@studms.ug.edu.pl>
2023-12-09 19:05:25 +0100

    added description to function

--- src/Services/UpdateRecords/enumerateData.service.ts
+++ src/Services/UpdateRecords/enumerateData.service.ts
@@ -4,6 +4,14 @@ type UpdateOrCreateFunction<RecordType, CompareType> = (
     compareValues: CompareType
 ) => Promise<void>;
 
+/**
+ *
+ * @param records - Records fetched from a scraper that will be compared to existing records.
+ * @param compareValues - Values used for comparison to detect changes in them.
+ * @param updateOrCreateData - Function used to compare, update or create data
+ * @param generateComparisonObject - It returns an object representing the unique identifier/key to search records.
+ */
+
 export const enumerateData = <RecordType, CompareType>(
     records: RecordType[],
     compareValues: CompareType,
